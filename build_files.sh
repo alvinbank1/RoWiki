efaff843806628e767df45554f52c31382178937
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2023-01-05 18:01:08 +0900

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,10 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 -m pip install pysqlite3
+LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib -L/usr/local/opt/sqlite/lib" \
+CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include -I/usr/local/opt/sqlite/include" \
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH} /usr/local/opt/zlib/lib/pkgconfig" \
+pyenv install 3.6.8
 yum install sqlite-devel -y
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
 python3.9 app.py
