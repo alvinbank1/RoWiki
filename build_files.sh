3eb350826f72318b92824627e710ce122e4341d0
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2023-01-05 17:58:28 +0900

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install pysqlite3
 yum install sqlite-devel -y
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
 python3.9 app.py
